c7352a244089621464cc7209a49deb0fb63425d6
Ahmiii <ahmii.shaikh18@gmail.com>
2020-09-15 19:22:45 +0500

    chat header title comming from firebase backend

--- src/components/ChatRoom/ChatHeader/ChatHeader.jsx
+++ src/components/ChatRoom/ChatHeader/ChatHeader.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import "./ChatHeader.css";
 import StarBorderOutlinedIcon from "@material-ui/icons/StarBorderOutlined";
 import InfoOutlinedIcon from "@material-ui/icons/InfoOutlined";
-const ChatHeader = () => {
+const ChatHeader = (props) => {
+  const { channelName } = props;
   return (
     <div className="chat__header">
       <div className="chat__headerLeft">
         <h4 className="chat__channelName">
-          <strong>#general</strong>
+          <strong>#{channelName?.name}</strong>
           <StarBorderOutlinedIcon />
         </h4>
       </div>
--- src/container/ChatRooms/ChatRooms.jsx
+++ src/container/ChatRooms/ChatRooms.jsx
@@ -1,10 +1,23 @@
-import React from "react";
-import Chatroom from "../../components/ChatRoom/Chatroom";
+import React, { useState, useEffect } from "react";
+import ChatroomHeader from "../../components/ChatRoom/ChatHeader/ChatHeader";
+import { useParams } from "react-router-dom";
+import db from "../../config/firebase";
 import "./ChatRooms.css";
 const ChatRooms = () => {
+  const roomId = useParams();
+  const [roomDetails, setroomDetails] = useState(null);
+
+  useEffect(() => {
+    if (roomId) {
+      db.collection("rooms")
+        .doc(roomId.roomId)
+        .onSnapshot((snapShot) => setroomDetails(snapShot.data()));
+    }
+  }, [roomId]);
+
   return (
     <div className="chat">
-      <Chatroom />
+      <ChatroomHeader channelName={roomDetails} />
     </div>
   );
 };

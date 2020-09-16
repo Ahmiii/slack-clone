5b9d83a3d5a53484b6c6c716412f955967640a46
Ahmiii <ahmii.shaikh18@gmail.com>
2020-09-16 20:17:15 +0500

    adding gogle signIn authentication

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./components/Header/Header";
 import SideBar from "./container/SideBar/SideBar";
 import ChatRooms from "./container/ChatRooms/ChatRooms";
+import Login from "./authentication/Login/Login";
 import { Route, Switch } from "react-router-dom";
 import "./App.css";
 function App() {
+  const [user, setuser] = useState(null);
+  if (!user) {
+    return <Login />;
+  }
   return (
     <div className="app">
       <Header />

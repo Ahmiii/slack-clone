430a765b52dc96c3c6488b5ceae6346fda656f56
Ahmiii <ahmii.shaikh18@gmail.com>
2020-09-23 15:32:34 +0500

    some changes in styling

--- src/components/ChatRoom/ChatBody/ChatBody.jsx
+++ src/components/ChatRoom/ChatBody/ChatBody.jsx
@@ -8,12 +8,12 @@ const ChatBody = (props) => {
       <div className="chat__messages">
         <img src={userImage} alt="faraz" />
         <div className="message__info">
-          <h4>
+          <h5>
             {user}{" "}
             <span className="message__timestamp">
               {new Date(timestamp?.toDate()).toUTCString()}
             </span>
-          </h4>
+          </h5>
           <p>{message}</p>
         </div>
       </div>
